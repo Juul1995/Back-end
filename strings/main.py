58fa4fbf1370c458b432ab43b375a0bcee68a57a
Julia Constant <you@example.com>
2021-07-29 22:43:04 +0200

    first commit

---
+++ strings/main.py
@@ -0,0 +1,30 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+player_0 = 'Ruud Gullit'
+player_1 = 'Marco van Basten'
+
+goal_0 = 32
+goal_1 = 54 
+
+scorers = player_0 +" "+ str(goal_0) + ', ' +player_1 +" "+ str(goal_1)
+report = f"{player_0} scored in the {goal_0}nd minute \n{player_1} scored in the {goal_1}th minute"
+
+player = 'Wim Kieft'
+first_name = player[:player.find(" ")]
+last_name = player[player.rfind(" ") + 1:]
+last_name_len = len(player[player.rfind(" ") + 1:])
+name_short = first_name[0] + '.'+ " " + last_name
+chant = (first_name +'! ')*len(first_name) 
+good_chant = chant[0] != " " 
+
+print(scorers)
+print(report)
+print (name_short)
+print (chant)
+print (good_chant)
+
+
+
